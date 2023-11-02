f1cfc7fa674e74b3c76231feaa79ca7f6d496d1e
Nweze Charles <104150471+EastBaby043@users.noreply.github.com>
2023-11-02 12:21:20 +0100

    css update

---
+++ script.js
@@ -0,0 +1,51 @@
+// const aboutBtn = document.getElementById('about-btn');
+// const aboutPage = document.querySelector('.about-page');
+
+// aboutBtn.addEventListener('click', () => {
+//   aboutPage.scrollIntoView({
+//     behavior: 'smooth',
+//   });
+// });
+
+
+
+
+
+// const aboutBtn = document.getElementById('about-btn');
+// const aboutSection = document.querySelector('.about-page');
+
+// aboutBtn.addEventListener('click', () => {
+//   aboutSection.scrollIntoView({
+//     behavior: 'smooth',
+//   });
+// });
+
+
+
+
+
+
+
+// const aboutBtn = document.getElementById('about-btn');
+// const aboutSection = document.querySelector('.about-page');
+
+// aboutBtn.addEventListener('click', () => {
+//   // Prevent the default behavior of the link. 
+//    event.preventDefault();
+
+//   // Scroll to the "about-page" with a smooth animation.
+//   window.scrollTo({
+//     top: aboutSection.offsetTop,
+//     behavior: 'smooth',
+//   });
+// });
+
+
+
+
+
+
+// document.getElementById("about-btn").addEventListener("click", function() {
+//     // Scroll to the "about-section" when the button is clicked
+//     document.getElementsByClassName("about-page").scrollIntoView({ behavior: "smooth" });
+// });
